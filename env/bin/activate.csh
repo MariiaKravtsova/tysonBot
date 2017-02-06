994de8218e2b16bcf8537583e2ab0132bf760e66
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-02-05 23:59:16 -0500

    Getting tweets from Neil

--- env/bin/activate.csh
+++ env/bin/activate.csh
@@ -7,7 +7,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/mkravtsova/Github/tysonBot/env"
+setenv VIRTUAL_ENV "/home/kravtsova/Github/tysonBot/env"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
---
+++ getTweets.py
@@ -0,0 +1,32 @@
+import time
+import json
+from api import getAPI
+
+REQUEST_DELAY = 7
+MAX_REQUESTS = 7
+
+def main():
+    try:
+        user = '@neiltyson'
+        api = getAPI()
+        tweetResults = []
+
+        tweetIndex = api.user_timeline(screen_name=user, count=1)[0].id
+        time.sleep(REQUEST_DELAY)
+        for request in range(MAX_REQUESTS):
+            tweets = api.user_timeline(screen_name=user, include_retweets=False, max_id=tweetIndex)
+            for tweet in tweets:
+                tweetResults.append(tweet.text)
+                tweetIndex = tweet.id
+            time.sleep(REQUEST_DELAY)
+
+    except IndexError:
+        print("Missing Twitter Handle")
+    except Exception as e:
+        print(e)
+    finally:
+        with open('{}Tweets'.format(user), 'w') as saveFile:
+            json.dump(tweetResults, saveFile)
+
+if __name__ == '__main__':
+    main()
